25a50fba477dbb90fe9d95992ddaed2ff9941643
Leandro de Azevedo Souza <74275422+alfa-leo-1@users.noreply.github.com>
2021-06-14 22:26:28 -0300

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,30 @@
+#Tuplas
+#estrutura de dados linear , quase igual a lista
+#diferene da lista porque ela não pode mudar os valor criados
+#entre parente e separado por aspas e virgulas
+#Tuplas e uma lista imutavel 
+
+aluno = ('Marcos',1.68,20) #nome,altura,idade
+print(aluno)
+print((alunos[0])#vem o indice do elemento zero
+
+      #tenho que elimiar os print para funcionar em python
+#estou atribuindo 
+nome = aluno[0]
+altura = aluno[1]
+idade = aluno[2]      
+print(nome)
+print(idade)
+print(altura)      
+
+
+#outro exemplo
+string = 'Tudo bem?'
+tuplas = (40,33,22,53,98,10)
+lista = [25,44,99]
+
+#exemplos: impirmindo letra por letra
+      nome = "Leanseo"
+      for x in nome:
+      print(x)#nome expresso letra por letra linha a linha
+      
