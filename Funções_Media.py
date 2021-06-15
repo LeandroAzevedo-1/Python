5a9f70520295bffe74a95b6dbd7d0b18cbded5b4
Leandro de Azevedo Souza <74275422+alfa-leo-1@users.noreply.github.com>
2021-06-14 22:22:22 -0300

    Add files via upload

---
+++ Funções_Media.py
@@ -0,0 +1,29 @@
+#Funções - trecho de código damos nomes passamos alguns componentes
+#funções tras vanges, facilde escrever e ler .
+#permite a reutilização de código .
+#sintaxe basica começando com def nomesdafunção e os paremetros exemplos
+
+#Funçao que não recebe parâmetro
+def exibemsg():
+    print("Crinado Funções")# retorna essa mensagem entre aspas
+    
+exibemsg() # chamando a função
+
+#Função com Parâmetro
+#Cria uma função que calcule a média aritmética de 3 números
+
+#Função média 
+def media(n1,n2,n3):
+    calcMedia =(n1+n2+n3)/3
+    print(calcMedia)
+media(3,7,9)#chamandoa a função os valos ta de forma fixa mais pode pedir p usuário
+
+
+#Funçao media pedindo para o usuario digitar o número
+def media(n1,n2,n3):
+    calcMedia =(n1+n2+n3)/3
+    print(calcMedia)
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+media(num1,num2,num3)#passamos entre parâmetro as variaveis digitada pelo usuario.
