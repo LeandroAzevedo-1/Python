2a8e4e40c300c0873beb4a9c41b4f718c1f03628
Leandro de Azevedo Souza <74275422+alfa-leo-1@users.noreply.github.com>
2021-06-14 22:32:13 -0300

    Add files via upload

---
+++ Multiplicação_lista.py
@@ -0,0 +1,8 @@
+listaNumeros = [5,4,3,8,7]
+soma = 0
+multiplica = 1
+i = 0
+while i < len(listaNumeros):
+    multiplica = multiplica*listaNumeros[i]
+    i = i+1
+print("multiplicacao dos numeross da lista: ", multiplica)    
